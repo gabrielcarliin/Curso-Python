ff36df8860302745dd362f936e844b700187f183
gabrielcarliin <75861006+gabrielcarliin@users.noreply.github.com>
2024-06-17 22:14:45 -0300

    Finalização da aula

--- PythonAulas/aula12a.py
+++ PythonAulas/aula12a.py
@@ -2,7 +2,10 @@
 
 nome = str(input('Qual é seu nome?'))
 
-if nome ==  'Gabriel':
+if nome == 'Gabriel':
     print('Que nome bonito!')
+elif nome == 'Maria' or nome == 'Lenita' or nome == 'Filipe':
+    print('Seu nome é bem popular no brasil.')
 else:
-    print('Que nome comum!')
+    print('Que nome comum...')
+print(f'Tenha um bom dia, {nome}!')
