38c64a76b341501deea355f6578d8f35e8630f2e
gabrielcarliin <75861006+gabrielcarliin@users.noreply.github.com>
2024-06-27 22:09:30 -0300

    Aula 13a - repetição for

---
+++ PythonAulas/aula13a.py
@@ -0,0 +1,15 @@
+print('Contagem de 0 a 5')
+for c in range(0, 6):
+    print(c)
+
+print('Contagem de 1 a 6')
+for c in range(1, 7):
+    print(c)
+
+print('Contagem de 6 a 1')
+for c in range(6, 0, -1):
+    print(c)
+
+print('Contagem de 0 a 6 pulando 2')
+for c in range(0, 7, 2):
+    print(c)
