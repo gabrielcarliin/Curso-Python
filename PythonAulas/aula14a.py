3f5900f883fac3c1d982efac7d2f22b1343f92da
gabrielcarliin <75861006+gabrielcarliin@users.noreply.github.com>
2024-07-25 20:18:39 -0300

    Aula 14a - While

--- PythonAulas/aula14a.py
+++ PythonAulas/aula14a.py
@@ -1,6 +1,13 @@
-c = 1
+"""c = 1
 while c < 10:
     print(c)
     c += 1
 
+print('Fim!')"""
+
+r = 'S'
+
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Deseja continuar? (S/N) ')).upper()
 print('Fim!')
